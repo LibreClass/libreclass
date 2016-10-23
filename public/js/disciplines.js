5c5e571cabf874e26b697a961e3ff8fc7f866ab6
Ladislau <caio.ladislau@sysvale.com>
2016-10-23 00:52:47 -0300

    Padrões de codifição

--- public/js/disciplines.js
+++ public/js/disciplines.js
@@ -7,44 +7,26 @@ $(document).ready(function() {
   });
 
   $("#select-course select").change(function() {
-    //$("#new-discipline select[name='course']").val($(this).val());
-
-    //~ alert("load start");
     $("#list-disciplines").load("/disciplines/list", {
       "course": $(this).val()
     }, function() {
       $(".trash").click(trash);
-      //      $(".trash").click(function(){
-      //        var key   = $(this).closest(".data").attr("key");
-      //
-      //        $("#form-trash input[name=\"input-trash\"]").val(key);
-      //        $("#form-trash").attr("action", document.URL + "/delete");
-      //
-      //        $.confirm("Tem certeza que deseja excluir?", function(){
-      //          $("#form-trash").submit();
-      //        });
-      //      });
-
-      //   $(".discipline-trash").click(function(){
-      //     var discipline = $(this).closest(".discipline").attr("discipline");
-      //     $("#delete-discipline input[name='discipline']").val(discipline);
-      //     $("#delete-discipline").submit();
-      //   });
 
       $(".discipline-edit").click(function() {
         var discipline = $(this).closest(".data").attr("key");
-        var periods = [],
-          courses = [];
+        var periods = [];
+        var courses = [];
         $("#new-discipline input[name='discipline']").val(discipline);
         $("#block").hide();
         $("#title-discipline").text("Editar Disciplina");
 
-        for (var i = 0; i < $("#new-discipline #period option").length; i++)
+        for (var i = 0; i < $("#new-discipline #period option").length; i++) {
           periods[i] = $("#new-discipline #period option").eq(i).attr("value");
+        }
 
-        for (var i = 0; i < $("#new-discipline #course option").length; i++)
+        for (var i = 0; i < $("#new-discipline #course option").length; i++) {
           courses[i] = $("#new-discipline #course option").eq(i).attr("value");
-
+        }
 
         $.getJSON("/disciplines/edit", {
           "discipline": discipline,
@@ -67,13 +49,13 @@ $(document).ready(function() {
       "course": $(this).val()
     }, function(data) {
       $("#new-discipline select[name='period']").html("");
-      for (var i = 0; i < data.length; i++)
+      for (var i = 0; i < data.length; i++) {
         $("#new-discipline select[name='period']").append($("<option>", {
           value: data[i].id,
           html: data[i].name
         }));
+      }
       console.log(data);
-
     });
   }).change();
 
