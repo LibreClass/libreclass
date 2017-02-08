2dac903a9752072e35f45623b767d5ba56af9be2
Caio Ladislau <caio.ladislau@sysvale.com>
2017-02-08 14:40:41 -0300

    Inclui variáveis de ambiente

--- app/config/mail.php
+++ app/config/mail.php
@@ -4,13 +4,13 @@ return array(
 	/*
   | Supported: "smtp", "mail", "sendmail", "mailgun", "mandrill", "log"
   */
-  'driver'     => 'smtp',
-  'host'       => 'mail.libreclass.com',
-  'port'       => 25,
-  'from'       => array('address' => "contato@libreclass.com", 'name' => "LibreClass"),
-  'encryption' => 'tls',
-  'username'   => "contato@libreclass.com",
-  'password'   => "SECRET",
+  'driver'     => $_ENV['EMAIL_DRIVER'],
+  'host'       => $_ENV['EMAIL_HOST'],
+  'port'       => $_ENV['EMAIL_PORT'],
+  'from'       => array('address' => $_ENV['EMAIL_FROMADD'], 'name' => $_ENV['EMAIL_FROMNAM']),
+  'encryption' => $_ENV['EMAIL_ENC'],
+  'username'   => $_ENV['EMAIL_UNAME'],
+  'password'   => $_ENV['EMAIL_PASS'],
   'sendmail'   => '/usr/sbin/sendmail -bs',
   'pretend'    => false,
 );
