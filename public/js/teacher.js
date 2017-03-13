e27aca0cf14e4f474c3c9d6db87a54f4d9a68544
Caio Ladislau <caiolaszlo@gmail.com>
2017-03-13 15:16:37 -0300

    Modifica estrutura do código conforme Drupal Coding Standards

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,4 +1,5 @@
 $(function() {
+
   $(".invite-teacher").click(function() {
     var teacher = $(this).closest("ul").attr("data");
     $("#name-invite-teacher").text($(this).closest("ul").attr("data-name"));
@@ -33,52 +34,48 @@ $(function() {
     var teacher = $(this).closest(".data").attr("data");
     //alert(teacher);
     $.getJSON("/user/infouser", {
-        "user": teacher
-    })
-      .done(function(data){
-        $("#formAddTeacher input[name='teacher']").val(teacher);
-        $("#formAddTeacher input[name='enrollment']").val(data.enrollment);
-        $("#formAddTeacher input[name='name']").val(data.name);
-        $("#formAddTeacher input[name='formation']").val(data.formation);
-        $("#modalAddTeacher").modal();
-
-      })
-      .fail(function() {
-        $("#modalAlert .modal-body").html("<span class='text-center'>Não foi possível realizar a operação. Se o problema persistir contate o <a href='mailto:suporte@sysvale.com'>suporte</a></span>");
-        $("#modalAlert").modal();
-      });
-
+      "user": teacher
+    }).done(function(data) {
+      $("#formAddTeacher input[name='teacher']").val(teacher);
+      $("#formAddTeacher input[name='enrollment']").val(data.enrollment);
+      $("#formAddTeacher input[name='name']").val(data.name);
+      $("#formAddTeacher input[name='formation']").val(data.formation);
+      $("#modalAddTeacher").modal();
+    }).fail(function() {
+      $("#modalAlert .modal-body").html("<span class='text-center'>Não foi possível realizar a operação. Se o problema persistir contate o <a href='mailto:suporte@sysvale.com'>suporte</a></span>");
+      $("#modalAlert").modal();
+    });
   });
 
-    $("#modal-body", "#modalAddTeacherDiscipline").load("/bind/list", {
-      teacher: $("#modalAddTeacherDiscipline").attr("teacher")
-    }, function(){
-      $("#modalAddTeacherDiscipline input:checkbox").click(function(){
-        $.post("/bind/link", {
-          user: $("#modalAddTeacherDiscipline").attr("teacher"),
-          discipline: $(this).attr("value"),
-          bind: $(this).is(":checked")
-        });
+  $("#modal-body", "#modalAddTeacherDiscipline").load("/bind/list", {
+    teacher: $("#modalAddTeacherDiscipline").attr("teacher")
+  }, function(){
+    $("#modalAddTeacherDiscipline input:checkbox").click(function(){
+      $.post("/bind/link", {
+        user: $("#modalAddTeacherDiscipline").attr("teacher"),
+        discipline: $(this).attr("value"),
+        bind: $(this).is(":checked")
       });
-      $("#modalAddTeacherDiscipline #search").keyup(function(){
-        var search = $(this).val().toLowerCase();
-        checkbox = $("#modalAddTeacherDiscipline .checkbox");
-        for( i = 0 ; i < $(checkbox).length ; i++ ) {
-          text = $(checkbox).eq(i).text().toLowerCase();
-          if(text.indexOf(search) != -1)
-            $(checkbox).eq(i).show();
-          else
-            $(checkbox).eq(i).hide();
-        }
-      })
     });
+    $("#modalAddTeacherDiscipline #search").keyup(function(){
+      var search = $(this).val().toLowerCase();
+      checkbox = $("#modalAddTeacherDiscipline .checkbox");
+      for( i = 0 ; i < $(checkbox).length ; i++ ) {
+        text = $(checkbox).eq(i).text().toLowerCase();
+        if(text.indexOf(search) != -1)
+          $(checkbox).eq(i).show();
+        else
+          $(checkbox).eq(i).hide();
+      }
+    })
+  });
 
   $(".add-teacher-discipline").click(function() {
     $("#modalAddTeacherDiscipline").modal();
   });
+
   $("#modalAddTeacherDiscipline").on("hide.bs.modal", function(){
     location.reload();
-  })
-
+  });
 
 });
