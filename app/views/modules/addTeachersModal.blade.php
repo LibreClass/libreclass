23efb1a259363f0195ef8fdbca44f6a810f29779
Caio Ladislau <caiolaszlo@gmail.com>
2017-03-13 15:08:32 -0300

    Inclui campo de email

--- app/views/modules/addTeachersModal.blade.php
+++ app/views/modules/addTeachersModal.blade.php
@@ -7,13 +7,21 @@
         </div>
         {{ Form::open(["id" => "formAddTeacher"]) }}
         <div class="modal-body">
-          
+
             {{ Form::hidden("teacher", null) }}
+
+            <div class="form-group">
+              {{ Form::label("email", "Email", ["class" => "control-label"]) }}
+              <span class="help-block">Informe o email do professor.</span>
+              {{ Form::email("email", null, ["class" => "form-control input-lg", "autofocus", "required"]) }}
+            </div>
+
             <div class="form-group">
               {{ Form::label("enrollment", "Matrícula", ["class" => "control-label"]) }}
               <span class="help-block">Informe a matrícula do professor.</span>
               {{ Form::text("enrollment", null, ["class" => "form-control input-lg", "autofocus", "required"]) }}
             </div>
+
             <div class="form-group">
               {{ Form::label("name", "Nome", ["class" => "control-label"]) }}
               <span class="help-block">Informe o nome completo do professor.</span>
@@ -32,13 +40,13 @@
                                             "Mestre",
                                             "Doutor"], null, ["class" => "form-control input-lg"]) }}
             </div>
-          
+
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
           <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Salvar</button>
         </div>
-        {{ Form::close() }} 
+        {{ Form::close() }}
       </div>
     </div>
-  </div>  
+  </div>
