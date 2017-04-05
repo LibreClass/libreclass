4daa51febd416d81d87e40ede91004560a91a1e1
Caio Ladislau <caiolaszlo@gmail.com>
2017-04-05 11:31:13 -0300

    Resolve pequenos problemas associados a padrões de codificação

--- app/routes.php
+++ app/routes.php
@@ -9,25 +9,25 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the Closure to execute when that URI is requested.
 |
-*/
+ */
 
 Route::controller('/censo', 'CensoController');
 
 // Erro ao detectar internet explorer
-Route::get('/ie', function(){
+Route::get('/ie', function () {
   return View::make("ie");
 });
 
 Route::controller('/classrooms', "ClassroomController");
 
 // Route::controller('LoL', "\student\DisciplinesController");
-Route::get('student', function() {
+Route::get('student', function () {
   return View::make("students.disciplines");
 });
 
 Route::controller('sync', "SyncController");
 
-Route::get('logout', function() {
+Route::get('logout', function () {
   Session::flush();
   return Redirect::guest("/");
 });
@@ -46,7 +46,7 @@ if (Session::get("user") == null) {
     Route::controller('classes/lessons', "LessonsController");
     Route::controller('classes/offers', "OffersController");
     Route::get('classes/units/report-unit/{idUnit}', "UnitsController@getReportUnit");
-    Route::controller('classes',"ClassesController");
+    Route::controller('classes', "ClassesController");
     Route::post('user/teacher/delete', "UsersController@postUnlink");
     Route::controller('user', "UsersController");
     Route::controller('import', "CSVController");
@@ -60,12 +60,12 @@ if (Session::get("user") == null) {
   if (Session::get("type") == "P") {
     Route::controller('courses', "CoursesController");
     Route::controller('classes/panel', "ClassesController");
-    Route::controller('classes',"ClassesController");
+    Route::controller('classes', "ClassesController");
     Route::controller('disciplines', "DisciplinesController");
     Route::controller('lectures/units', "UnitsController");
-    Route::controller('lectures',"LecturesController");
-    Route::controller('avaliable',"AvaliableController");
-    Route::controller('lessons',"LessonsController"); /* anotações de aula */
+    Route::controller('lectures', "LecturesController");
+    Route::controller('avaliable', "AvaliableController");
+    Route::controller('lessons', "LessonsController"); /* anotações de aula */
     Route::get('user/profile', "UsersController@getProfile");
     Route::get('user/student', "UsersController@getStudent");
     Route::post('user/student', "UsersController@postStudent");
