7db4f06af41c908c749f5b075a496db17a62c669
Caio Ladislau <caiolaszlo@gmail.com>
2017-04-11 11:25:18 -0300

    Inclui path dos binários

---
+++ app/config/packages/barryvdh/laravel-snappy/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+  'pdf' => array(
+    'enabled' => true,
+    'binary' => '../vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
+    'timeout' => false,
+    'options' => array(),
+  ),
+  'image' => array(
+    'enabled' => true,
+    'binary' => '../vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64',
+    'timeout' => false,
+    'options' => array(),
+  ),
+
+);
