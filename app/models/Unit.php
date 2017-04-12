d0790304c1a1302073686ed709d425ad3ef8ac4c
Caio Ladislau <caiolaszlo@gmail.com>
2017-04-12 11:44:52 -0300

    Retorna o resultado do aluno

--- app/models/Unit.php
+++ app/models/Unit.php
@@ -1,10 +1,12 @@
 <?php
 
-class Unit extends \Eloquent {
+class Unit extends \Eloquent
+{
   protected $table = "Units";
   protected $fillable = ['idOffer'];
 
-  public function offer() {
+  public function offer()
+  {
     return $this->belongsTo('Offer', 'idOffer');
   }
 
@@ -15,35 +17,42 @@ class Unit extends \Eloquent {
 
   public function getAverage($student)
   {
-    $exams  = Exam::where("idUnit", $this->id)->whereAval("A")->whereStatus("E")->get();
+    $exams = Exam::where("idUnit", $this->id)->whereAval("A")->whereStatus("E")->get();
     $attend = Attend::where("idUnit", $this->id)->where("idUser", $student)->first();
-    if ( !$attend ) return null;
-    $out    = [null, null];
-    $sum    = 0.;
+    if (!$attend) {
+      return null;
+    }
+
+    $out = [null, null];
+    $sum = 0.;
     $weight = 0.;
     // echo count($exams) . " - ";
-    foreach( $exams as $exam ) {
+    foreach ($exams as $exam) {
       $value = ExamsValue::where("idExam", $exam->id)->where("idAttend", $attend->id)->first();
       // echo " $value ";
-      if ( $value )
-        $sum += $value->value * ($this->calculation == "W" ? $exam->weight : 1); /* so multiplica pelo peso quando for média ponderada */
+      if ($value) {
+        $sum += $value->value * ($this->calculation == "W" ? $exam->weight : 1);
+      }
+      /* so multiplica pelo peso quando for média ponderada */
       $weight += $exam->weight;
     }
 
     // echo $this->calculation . " $sum ";
     /* tipo de calculo da média */
-    if ( $this->calculation == "A" and count($exams) )
-      $out[0] = $sum/count($exams);
-    elseif ( $this->calculation == "W" and $weight > 0 )
-      $out[0] = $sum/$weight;
-    elseif ( $this->calculation == "S" )
+    if ($this->calculation == "A" and count($exams)) {
+      $out[0] = $sum / count($exams);
+    } elseif ($this->calculation == "W" and $weight > 0) {
+      $out[0] = $sum / $weight;
+    } elseif ($this->calculation == "S") {
       $out[0] = $sum;
+    }
 
     $final = Exam::where("idUnit", $this->id)->whereAval("R")->first();
-    if ( $final ) {
+    if ($final) {
       $value = ExamsValue::where("idExam", $final->id)->where("idAttend", $attend->id)->first();
-      if ( $value )
+      if ($value) {
         $out[1] = $value->value;
+      }
     }
 
     // print_r($out);exit;
@@ -61,7 +70,8 @@ class Unit extends \Eloquent {
     return Lesson::where("idUnit", $this->id)->whereStatus("E")->orderBy("date", "asc")->orderBy("id", "asc")->get();
   }
 
-  public function countLessons() {
+  public function countLessons()
+  {
     return Lesson::where("idUnit", $this->id)->whereStatus("E")->count();
   }
 
