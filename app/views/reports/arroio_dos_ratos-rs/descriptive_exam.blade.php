2fdbad216c4f0e7a899a2d04eee1cae2f40ccf13
Caio Ladislau <caiolaszlo@gmail.com>
2017-04-12 15:54:23 -0300

    Exibe nome da disciplina e nome dos professores

--- app/views/reports/arroio_dos_ratos-rs/descriptive_exam.blade.php
+++ app/views/reports/arroio_dos_ratos-rs/descriptive_exam.blade.php
@@ -4,7 +4,6 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
 
 	<body>
@@ -15,30 +14,42 @@
 					<td style="width: 16%; padding-right: 15px;"><img style="width: 85%;" src="{{ public_path() }}/images/logo-arroio_dos_ratos-rs.jpg" class="img-responsive"></td>
 					<td style="width: 68%;" class="text-center">
 						<div style="width: 100%;">
-							<h4>{{ $institution->name }}</h4>
-							<h5>{{ $institution->street }}, {{ $institution->local }}</h5>
-							<h5>Código UEE: {{ $institution->uee }}</h5>
+							<h4>{{ $data['institution']->name }}</h4>
+							<h5>{{ $data['institution']->street }}, {{ $data['institution']->local }}</h5>
+							<h5>Código UEE: {{ $data['institution']->uee }}</h5>
 						</div>
 					</td>
-					<td style="width: 16%; padding-left: 15px;"><img src="{{ public_path() . $institution->photo }}" class="img-responsive"></td>
+					<td style="width: 16%; padding-left: 15px;"><img src="{{ public_path() . $data['institution']->photo }}" class="img-responsive"></td>
 				</tr>
 			</table>
 		</header>
 
 		<div>
 			<ol class="breadcrumb">
-        <li>{{ $unit->offer->discipline->period->course->name }}</li>
-        <li>{{ $unit->offer->discipline->period->name }}</li>
-        <li>{{ $unit->offer->getClass()->fullName() }}</li>
-        <li>Unidade {{ $unit->value }}</li>
+        <li>{{ $data['unit']->offer->discipline->period->course->name }}</li>
+        <li>{{ $data['unit']->offer->discipline->period->name }}</li>
+        <li>{{ $data['unit']->offer->getClass()->fullName() }}</li>
+        <li>Unidade {{ $data['unit']->value }}</li>
       </ol>
 		</div>
 
 		<div class="pdf">
 		  @foreach ($data['exams'] as $exam)
-				<h4><strong>Título da avaliação: </strong>{{ $exam['data']['title'] }}
+		  	<h5><strong>Disciplina: </strong> {{ $data['discipline'] }}</h5>
+		  	<h5>
+		  		@if (count($data['teachers']) == 1)
+			  		<strong>Professor(a): </strong>
+		  			<span>{{ $data['teachers'][0] }}</span>
+		  		@elseif (count($data['teachers']) > 1)
+		  			<strong>Professores(as): </strong>
+						@foreach ($data['teachers'] as $teacher)
+							<span>{{ $teacher }}</span>
+						@endforeach
+		  		@endif
+		  	<h5>
+				<h5><strong>Título da avaliação: </strong>{{ $exam['data']['title'] }}
 					<small>{{ date_format(date_create($exam['data']['date']), "d/m/Y") }}</small>
-				</h4>
+				</h5>
 				@if ($exam['data']['comments'])
 					<h5><strong>Descrição: </strong>{{ $exam['data']['comments'] }}</h5>
 				@endif
@@ -47,7 +58,7 @@
 						<tr>
 							<td>
 								<h5><b>{{ $d->student->name }} <small>{{ $d->approved == 'A' ? 'Aprovado' : 'Reprovado' }}</small></b></h5>
-								<h5><b>Aulas realizadas: {{ $unit->count_lessons }} / Faltas: {{ $d->student->absence }}</b></h5>
+								<h5><b>Aulas realizadas: {{ $data['unit']->count_lessons }} / Faltas: {{ $d->student->absence }}</b></h5>
 								<p>{{ $d->description }}</p>
 							</td>
 						</tr>
