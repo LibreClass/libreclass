b5c11ceee52b776b517d925007677aeeaa74ff63
Caio Ladislau <caiolaszlo@gmail.com>
2017-05-05 10:58:12 -0300

    Exibe frequência e permite visualização sem que sejam lançadas avaliações

--- app/views/reports/arroio_dos_ratos-rs/descriptive_exam.blade.php
+++ app/views/reports/arroio_dos_ratos-rs/descriptive_exam.blade.php
@@ -4,6 +4,37 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<style type="text/css" media="screen">
+			.vertical {
+				vertical-align: middle !important;
+				height: 120px;
+			}
+			.vertical-align {
+				vertical-align: middle !important;
+			}
+			.nowrap {
+				white-space: nowrap;
+			}
+			.limited-width {
+				max-width: 3em;
+			}
+			.rotate {
+			  text-align: center;
+				white-space: nowrap;
+				vertical-align: middle !important;
+				min-width: 1.5em;
+			}
+			.rotate div {
+				-moz-transform: rotate(-90.0deg);  /* FF3.5+ */
+				-o-transform: rotate(-90.0deg);  /* Opera 10.5 */
+				-webkit-transform: rotate(-90.0deg);  /* Saf3.1+, Chrome */
+				margin-left: -10em;
+				margin-right: -10em;
+			}
+			tr {
+				page-break-inside: avoid;
+			}
+		</style>
 	</head>
 
 	<body>
@@ -11,62 +42,105 @@
 		<header style="margin-bottom: 15px;">
 			<table>
 				<tr>
-					<td style="width: 16%; padding-right: 15px;"><img style="width: 85%;" src="{{ public_path() }}/images/logo-arroio_dos_ratos-rs.jpg" class="img-responsive"></td>
-					<td style="width: 68%;" class="text-center">
+					<td style="width: 10%; padding-right: 15px;"><img style="width: 85%;" src="{{ public_path() }}/images/logo-arroio_dos_ratos-rs.jpg" class="img-responsive"></td>
+					<td style="width: 80%;" class="text-center">
 						<div style="width: 100%;">
 							<h4>{{ $data['institution']->name }}</h4>
 							<h5>{{ $data['institution']->street }}, {{ $data['institution']->local }}</h5>
 							<h5>Código UEE: {{ $data['institution']->uee }}</h5>
 						</div>
 					</td>
-					<td style="width: 16%; padding-left: 15px;"><img src="{{ public_path() . $data['institution']->photo }}" class="img-responsive"></td>
+					<td style="width: 10%; padding-left: 15px;"><img src="{{ public_path() . $data['institution']->photo }}" class="img-responsive"></td>
 				</tr>
 			</table>
 		</header>
 
 		<div>
-			<ol class="breadcrumb">
-        <li>{{ $data['unit']->offer->discipline->period->course->name }}</li>
-        <li>{{ $data['unit']->offer->discipline->period->name }}</li>
-        <li>{{ $data['unit']->offer->getClass()->fullName() }}</li>
-        <li>Unidade {{ $data['unit']->value }}</li>
-      </ol>
+			<h5 class='text-center breadcrumb'>Informações do curso</h5>
 		</div>
 
-		<div class="pdf">
-		  @foreach ($data['exams'] as $exam)
-		  	<h5><strong>Disciplina: </strong> {{ $data['discipline'] }}</h5>
-		  	<h5>
-		  		@if (count($data['teachers']) == 1)
-			  		<strong>Professor(a): </strong>
-		  			<span>{{ $data['teachers'][0] }}</span>
-		  		@elseif (count($data['teachers']) > 1)
-		  			<strong>Professores(as): </strong>
-						@foreach ($data['teachers'] as $teacher)
-							<span>{{ $teacher }}</span>
-						@endforeach
-		  		@endif
-		  	<h5>
-		  	<h5><b>Aulas realizadas:</b> {{ $data['unit']->count_lessons }}</h5>
-				<h5><strong>Título da avaliação: </strong>{{ $exam['data']['title'] }}
-					<small>{{ date_format(date_create($exam['data']['date']), "d/m/Y") }}</small>
-				</h5>
-				@if ($exam['data']['comments'])
-					<h5><strong>Descrição: </strong>{{ $exam['data']['comments'] }}</h5>
-				@endif
-				<table class="table table-bordered">
-					@foreach ($exam['descriptions'] as $d)
-						<tr>
-							<td>
-								<h5><b>{{ $d->student->name }} <small>{{ $d->approved == 'A' ? 'Aprovado' : 'Reprovado' }}</small></b></h5>
-								<h5>Faltas: {{ $d->student->absence }}</b></h5>
-								<p>{{ $d->description }}</p>
-							</td>
-						</tr>
-					@endforeach
-				</table>
-			@endforeach
+		<div>
+			<div class='container small'>
+				<div class="row">
+					<div class="col-xs-4"><p><b>Tipo de ensino:</b> {{ $data['course']->type }}</p></div>
+					<div class="col-xs-4"><p><b>Modalidade:</b> {{ $data['course']->modality }}</p></div>
+					<div class="col-xs-4"><p><b>Submodalidade:</b> {{ $data['course']->name }}</p></div>
+				</div>
+				<div class="row">
+					<div class="col-xs-4"><p><b>Turma:</b> {{ $data['classe']->name }}</p></div>
+					<div class="col-xs-4"><p><b>Série:</b> {{ $data['period']->name }}</p></div>
+					<div class="col-xs-4"><p><b>Período Letivo:</b> {{ $data['classe']->class }}</p></div>
+				</div>
+			</div>
 		</div>
+
+		<div>
+			<h5 class='text-center breadcrumb'>Diário de Classe</h5>
+		</div>
+
+		<div class="small">
+			<table class="table table-bordered table-condensed">
+				<tr>
+					<th class="text-center vertical"><b>N#</b></th>
+					<th class="text-center vertical"><b>Aluno(a)</b></th>
+					@foreach ($data['lessons'] as $lesson)
+						<th class="rotate vertical"><div>{{ $lesson }}</div></th>
+					@endforeach
+					<th class="rotate vertical"><div>Faltas</div></th>
+				</tr>
+				@foreach ($data['students'] as $student)
+					<tr>
+						<td class="text-center">{{ $student->number }}</td>
+						<td class="nowrap">{{ trim($student->name) }}</td>
+						@foreach ($student->absences as $absence)
+							<td class="text-center">{{ $absence }}</td>
+						@endforeach
+						<td class="text-center">{{ $student->countAbsences }}</td>
+					</tr>
+				@endforeach
+			</table>
+		</div>
+
+		@if ($data['exams'] != null)
+			<div>
+				<h5 class='text-center breadcrumb'>Avaliações</h5>
+			</div>
+
+			<div class="pdf">
+			  @foreach ($data['exams'] as $exam)
+			  	<h5><strong>Disciplina: </strong> {{ $data['discipline'] }}</h5>
+			  	<h5>
+			  		@if (count($data['teachers']) == 1)
+				  		<strong>Professor(a): </strong>
+			  			<span>{{ $data['teachers'][0] }}</span>
+			  		@elseif (count($data['teachers']) > 1)
+			  			<strong>Professores(as): </strong>
+							@foreach ($data['teachers'] as $teacher)
+								<span>{{ $teacher }}</span>
+							@endforeach
+			  		@endif
+			  	<h5>
+			  	<h5><b>Aulas realizadas:</b> {{ $data['unit']->count_lessons }}</h5>
+					<h5><strong>Título da avaliação: </strong>{{ $exam['data']['title'] }}
+						<small>{{ date_format(date_create($exam['data']['date']), "d/m/Y") }}</small>
+					</h5>
+					@if ($exam['data']['comments'])
+						<h5><strong>Descrição: </strong>{{ $exam['data']['comments'] }}</h5>
+					@endif
+					<table class="table table-bordered">
+						@foreach ($exam['descriptions'] as $d)
+							<tr>
+								<td>
+									<h5><b>{{ $d->student->name }} <small>{{ $d->approved == 'A' ? 'Aprovado' : 'Reprovado' }}</small></b></h5>
+									<h5>Faltas: {{ $d->student->absence }}</b></h5>
+									<p>{{ $d->description }}</p>
+								</td>
+							</tr>
+						@endforeach
+					</table>
+				@endforeach
+			</div>
+		@endif
 
 	</body>
 
