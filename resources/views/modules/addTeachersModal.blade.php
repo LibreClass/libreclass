fd7f868f14c69740fd09031a5b298f2bc7f0e99d
scarlate <scarlate100@gmail.com>
2021-03-30 13:14:00 -0300

    Adicionad data e sexo no formulario de professor

--- resources/views/modules/addTeachersModal.blade.php
+++ resources/views/modules/addTeachersModal.blade.php
@@ -7,33 +7,46 @@
         </div>
         {{ Form::open(["id" => "formAddTeacher"]) }}
         <div class="modal-body">
-
-            {{ Form::hidden("teacher", null) }}
-            {{ Form::hidden("registered", null) }}
-
-            <div class="form-group">
-              {{ Form::label("email", "Email", ["class" => "control-label"]) }}
-              <span class="help-block">Informe o email do professor. <img class="spinner" height="25" src="/images/spinner.svg" alt="spinner"></span>
-              {{ Form::email("email", null, ["class" => "form-control", "autofocus", "required"]) }}
-              <span class="teacher-message text-info"><b>Este professor já está cadastrado no LibreClass e será vinculado à sua instituição.</b></span>
+          {{ Form::hidden("teacher", null) }}
+          {{ Form::hidden("registered", null) }}
+          <div class="form-group">
+            {{ Form::label("email", "*Email", ["class" => "control-label"]) }}
+            <span class="help-block">Informe o email do professor. <img class="spinner" height="25" src="/images/spinner.svg" alt="spinner"></span>
+            {{ Form::email("email", null, ["class" => "form-control", "autofocus", "required"]) }}
+            <span class="teacher-message text-info"><b>Este professor já está cadastrado no LibreClass e será vinculado à sua instituição.</b></span>
+          </div>
+          <div class="form-group">
+            {{ Form::label("enrollment", "*Matrícula", ["class" => "control-label"]) }}
+            <span class="help-block">Informe a matrícula do professor.</span>
+            {{ Form::text("enrollment", null, ["class" => "form-control", "autofocus", "required"]) }}
+          </div>
+          <div class="form-group">
+            {{ Form::label("name", "*Nome", ["class" => "control-label"]) }}
+            <span class="help-block">Informe o nome completo do professor.</span>
+            {{ Form::text("name", null, ["class" => "form-control", "autofocus", "required"]) }}
+          </div>
+          <div class="row">
+            <div class="col-md-5 col-xs-5">
+              <div class="form-group">
+                {{ Form::label("date-day", "Data de Nascimento: ")}}
+                <div class="form-inline">
+                  {{ Form::selectRange("date-day", 1, 31, null, ["class" => "form-control"]) }}
+                  {{ Form::selectRange("date-month", 1, 12, null, ["class" => "form-control"]) }}
+                  {{ Form::selectRange("date-year", date("Y"), date("Y")-100, null, ["class" => "form-control"]) }}
+                </div>
+              </div>
             </div>
-
-            <div class="form-group">
-              {{ Form::label("enrollment", "Matrícula", ["class" => "control-label"]) }}
-              <span class="help-block">Informe a matrícula do professor.</span>
-              {{ Form::text("enrollment", null, ["class" => "form-control", "autofocus", "required"]) }}
+            <div class="col-md-7">
+              <div class="form-group">
+                {{ Form::label("gender", "*Sexo") }}
+                {{ Form::select("gender", ['M'=> 'Masculino', 'F'=> 'Feminino'], null, ["class" => "form-control", 'required']) }}
+              </div>
             </div>
-
-            <div class="form-group">
-              {{ Form::label("name", "Nome", ["class" => "control-label"]) }}
-              <span class="help-block">Informe o nome completo do professor.</span>
-              {{ Form::text("name", null, ["class" => "form-control", "autofocus", "required"]) }}
-            </div>
-
-            <div class="form-group">
-              {{ Form::label("formation", "Formação Acadêmica", ["control" => "control-label"]) }}
-              <span class="help-block">Informe a formação acadêmica principal do professor.</span>
-              {{ Form::select("formation", ["Não quero informar",
+          </div>
+          <div class="form-group">
+            {{ Form::label("formation", "*Formação Acadêmica", ["control" => "control-label"]) }}
+            <span class="help-block">Informe a formação acadêmica principal do professor.</span>
+            {{ Form::select("formation", ["Não quero informar",
                                             "Ensino Fundamental",
                                             "Ensino Médio",
                                             "Ensino Superior Incompleto",
@@ -41,8 +54,7 @@
                                             "Pós-Graduado",
                                             "Mestre",
                                             "Doutor"], null, ["class" => "form-control", "required"]) }}
-            </div>
-
+          </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
