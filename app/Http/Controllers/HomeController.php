a69112c4c02e2ee1e720a6e09535d1ce42355aa7
Caio Ladislau <caio@ladislau.me>
2021-11-06 09:46:02 -0300

    Implementa método estático para atualização da sessão com nomenclatura de períodos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,11 +10,18 @@ class HomeController extends Controller
 		if (auth()->guest()) {
 			return view('home');
 		} else {
-			$config = UserConfig::getConfig();
-			session(['period' => $config->period]);
+			$this::updatePeriodSession();
 			return view('social.home');
 		}
 
+	}
+
+	/**
+	 * Atualiza session com nomenclatura de períodos
+	 */
+	public static function updatePeriodSession() {
+		$config = UserConfig::getConfig();
+		session(['period' => $config->period]);
 	}
 
 	public function ie()
