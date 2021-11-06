93b2547a62e0f659bdf3cb942f8cddf1b007886c
Caio Ladislau <caio@ladislau.me>
2021-11-06 09:48:41 -0300

    Implementa utilização do axios lib

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
+window.Axios = require('axios');
+
 window.Vue = require('vue');
+
 import Vue from 'vue';
+
 import LibreclassComponents from '@libreclass/libreclass-components';
+
 Vue.use(LibreclassComponents);
 
 const app = new Vue({
