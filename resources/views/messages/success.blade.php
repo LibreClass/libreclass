6960eb7ec2c9f0277fba4b0dedbd5a77f1916613
Caio Ladislau <caio@ladislau.me>
2021-11-17 14:37:05 -0300

    Implementa coding standards.

--- resources/views/messages/success.blade.php
+++ resources/views/messages/success.blade.php
@@ -1,25 +1,25 @@
 @if(session("success"))
-  <div id="modalSuccess" class="modal fade" tabindex="-1" role="dialog Modal Success" data-backdrop="true" aria-labelledby="myModalSuccess" aria-hidden="true">
+<div id="modalSuccess" class="modal fade" tabindex="-1" role="dialog Modal Success" data-backdrop="true" aria-labelledby="myModalSuccess" aria-hidden="true">
     <div class="modal-dialog modal-sm">
-      <div class="modal-content">
-        <div class="modal-header modal-header-success">
-          <i class="fa fa-check fa-lg"></i>
-          <span><b> &nbsp;Sucesso</b></span>
-          <button type="button" class="close text-danger" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <div class="modal-content">
+            <div class="modal-header modal-header-success">
+                <i class="fa fa-check fa-lg"></i>
+                <span><b> &nbsp;Sucesso</b></span>
+                <button type="button" class="close text-danger" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="text-center">
+                    <span class="text-center">{!! session("success") !!}</span>
+                </div>
+                <br>
+                <div class="text-right">
+                    <button class="btn btn-default" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
         </div>
-        <div class="modal-body">
-          <div class="text-center">
-            <span class="text-center">{!! session("success") !!}</span>
-          </div>
-          <br>
-          <div class="text-right">
-            <button class="btn btn-default" data-dismiss="modal">Fechar</button>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-  <script>
+</div>
+<script>
     $("#modalSuccess").modal();
-  </script>
+</script>
 @endif
