0d984f1c9df79fc103edb9a4b09dcfb4cb3235ea
Caio Ladislau <caio@ladislau.me>
2021-11-17 18:09:16 -0300

    Implementa coding standards.

--- resources/views/modules/addTeacherDiscipline.blade.php
+++ resources/views/modules/addTeacherDiscipline.blade.php
@@ -1,15 +1,15 @@
 <div class="modal fade" id="modalAddTeacherDiscipline" tabindex="-1" role="Modal Add Teacher Discipline" aria-labelledby="modalAddTeacherDiscipline" aria-hidden="true" teacher='{{ encrypt($profile->id) }}'>
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h3 class="text-blue"><b><i class="icon-teacher"></i><span class="modal-title"></span>Disciplinas que pode atuar</b></h3>
-      </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="text-blue"><b><i class="icon-teacher"></i><span class="modal-title"></span>Disciplinas que pode atuar</b></h3>
+            </div>
 
-      <div id="modal-body">
-        Carregando...
-      </div>
+            <div id="modal-body">
+                Carregando...
+            </div>
 
+        </div>
     </div>
-  </div>
 </div>
