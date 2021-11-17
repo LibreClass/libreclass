80d9bf2de5991f79ee2196044ffdfd0e56125764
Caio Ladislau <caio@ladislau.me>
2021-11-17 18:04:51 -0300

    Implementa coding standards.

--- resources/views/offers/ementa.blade.php
+++ resources/views/offers/ementa.blade.php
@@ -1,27 +1,28 @@
   <div class="modal fade" id="modalEmenta" tabindex="-1" role="Modal Ementa da Oferta" aria-labelledby="modalEmenta" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="modalEmentaLabel"></h3>
-        </div>
-        
-        <div id="modalEmentaBody" class="modal-body">
-          
-          <div class="row">
-            <div class="col-md-12 ementa">
-              
-            </div>
+      <div class="modal-dialog">
+          <div class="modal-content">
+
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <h4 class="modal-title" id="modalEmentaLabel">
+                      </h3>
+              </div>
+
+              <div id="modalEmentaBody" class="modal-body">
+
+                  <div class="row">
+                      <div class="col-md-12 ementa">
+
+                      </div>
+                  </div>
+
+              </div>
+
+
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Sair</button>
+              </div>
+
           </div>
-          
-        </div>
-        
-        
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Sair</button>
-        </div>
-        
       </div>
-    </div>
-  </div>  
+  </div>
